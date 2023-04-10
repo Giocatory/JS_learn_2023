6bbad6e68d7d3c708fc6acb9907bb89e87e0feb8
Giocatory <nivolyas@gmail.com>
2023-04-10 23:21:44 +0800

    10.04.23

---
+++ scripts/closure.js
@@ -0,0 +1,20 @@
+const add = (function () {
+    let count = 0;
+    
+    const nested = function (){
+        return count += 1;
+    };
+
+    return nested;
+})();
+
+
+for (let index = 0; index < 3; index++) {
+    console.log(`Count is: ${add()}`);
+}
+
+/*
+Count is: 1
+Count is: 2
+Count is: 3
+*/
