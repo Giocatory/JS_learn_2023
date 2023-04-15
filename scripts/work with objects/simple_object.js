516004ea99ebe3a3a75eb5e060c8c6a1cdaca9e4
Giocatory <nivolyas@gmail.com>
2023-04-15 21:01:42 +0800

    simple object and for..in for this object

--- scripts/work with objects/simple_object.js
+++ scripts/work with objects/simple_object.js
@@ -9,18 +9,21 @@ let user = {
     }
 }
 
-console.log(`User ${user.name}: position admin ${user.isAdmin}`); // User Mikhail: position admin true
-console.log(`In company position ${user.employee.position} with salary ${user?.['employee']?.['salary']}`); // In company position middle with salary 200000
+console.log(`User ${user?.name}: position admin ${user?.isAdmin}`); // User Mikhail: position admin true
+console.log(`In company position ${user?.employee?.position} with salary ${user?.['employee']?.['salary']}`); // In company position middle with salary 200000
 
-console.log(`\nTypeOf user: ${typeof user.employee}\n`);
+console.log(`\nTypeOf user: ${typeof user}\n`);
 
 for(let value in user) {
-    if(`${user[value]}` === "[object Object]"){
+    
+    if(user[value].toString() === "[object Object]"){
         console.log(`${value}:`);
         for (let subValue in user[value]){
             console.log(`\t${subValue}: ${user[value][subValue]}`);
         }
         continue;
     }
+    
     console.log(`${value}: ${user[value]}`);
 }
+
