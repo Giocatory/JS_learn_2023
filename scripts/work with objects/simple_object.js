ef7b089f4e9363a8a5d6030540499845f854cfe2
Giocatory <nivolyas@gmail.com>
2023-04-15 14:45:06 +0800

    15.04.23

---
+++ scripts/work with objects/simple_object.js
@@ -0,0 +1,26 @@
+'use strict';
+
+let user = {
+    name: 'Mikhail',
+    isAdmin: true,
+    employee:{
+        position: 'middle',
+        salary: 200000,
+    }
+}
+
+console.log(`User ${user.name}: position admin ${user.isAdmin}`); // User Mikhail: position admin true
+console.log(`In company position ${user.employee.position} with salary ${user?.['employee']?.['salary']}`); // In company position middle with salary 200000
+
+console.log(`\nTypeOf user: ${typeof user.employee}\n`);
+
+for(let value in user) {
+    if(`${user[value]}` === "[object Object]"){
+        console.log(`${value}:`);
+        for (let subValue in user[value]){
+            console.log(`\t${subValue}: ${user[value][subValue]}`);
+        }
+        continue;
+    }
+    console.log(`${value}: ${user[value]}`);
+}
