4fad689da92eacf39b7036a0c268d3456c50c1c2
Giocatory <nivolyas@gmail.com>
2023-04-16 19:04:50 +0800

    array methods

--- scripts/data types/arraysNext.js
+++ scripts/data types/arraysNext.js
@@ -1,9 +1,9 @@
 'use strict';
 
 let users = [
-    {id : 1, name : 'Mike'},
-    {id : 2, name : 'John'},
-    {id : 3, name : 'Peter'},
+    {id : 1, name : 'Mike', age : 35},
+    {id : 2, name : 'John', age : 34},
+    {id : 3, name : 'Peter', age : 32},
 ]
 
 // method find()
@@ -17,4 +17,20 @@ console.log(userName); // Mike
 // method filter()
 
 let someUsers = users.filter(user => user.id < 3); // return array of users
-console.log(someUsers); // [ { id: 1, name: 'Mike' }, { id: 2, name: 'John' } ]
+console.log(someUsers); // [ { id: 1, name: 'Mike', age: 35 }, { id: 2, name: 'John', age: 34 } ]
+
+// method map
+
+let userNames = users.map(user => user.name);
+console.log(userNames); // [ 'Mike', 'John', 'Peter' ]
+
+// method reduce
+
+let middleUserAges = (
+                        users
+                            .map(user => user.age)
+                            .reduce((sum, n) => sum + n) 
+                            / users.length
+                     ).toFixed(0); // 33.666666666666664 => 34
+
+console.log(middleUserAges); // 34
