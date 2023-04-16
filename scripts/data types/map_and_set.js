aff34e4643d69e2b121380b2d46c65e45cfa4a1b
Giocatory <nivolyas@gmail.com>
2023-04-16 19:41:34 +0800

    map and set

--- scripts/data types/map_and_set.js
+++ scripts/data types/map_and_set.js
@@ -44,5 +44,12 @@ for (let user of set) {
 
 // experiment
 
-let nums = Array.from(new Set([1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 3, 3, 5, 6, 2, 9]));
-console.log(nums); // [1, 2, 3, 4, 5, 6, 7, 8, 9]
+let nums = Array.from(new Set([1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 3, 3, 5, 6, 2, 9, 11]));
+console.log(nums); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 11]
+
+for(let i = 0; i < nums.length; i++) {
+    if(nums[i] === 11) {
+        nums[i] = 0;
+    }
+}
+console.log(nums);
